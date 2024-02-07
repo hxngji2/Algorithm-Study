f0166886379df2d5fd9fcbd51f284f48a3a3b77a
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-02-07 16:54:04 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double first = scanner.nextInt();
+        double second = scanner.nextInt();
+
+        System.out.println(first/second);
+    }
+}
