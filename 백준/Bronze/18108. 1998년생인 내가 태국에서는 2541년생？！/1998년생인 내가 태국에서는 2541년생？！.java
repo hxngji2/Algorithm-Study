fe43c4a370a3e2696c800d3dbf37b3406e614a08
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-02-08 11:58:22 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int buddhism = scanner.nextInt();
+
+        int adYear = buddhism - 543;
+
+        System.out.println(adYear);
+    }
+}
