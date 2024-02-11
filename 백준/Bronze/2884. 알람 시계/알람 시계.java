88bc1da7627a63a6f099241c392877daf02cf50e
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-02-11 17:24:13 +0900

    [Bronze III] Title: 알람 시계, Time: 148 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m >= 45) {
+            m = m - 45;
+            System.out.println(h + " " + m);
+        } else {
+            if (h == 0) {
+                h = 24;
+                h--;
+                m = 60 - 45 + m;
+                System.out.println(h + " " + m);
+            } else {
+                h--;
+                m = 60 - 45 + m;
+                System.out.println(h + " " + m);
+            }
+
+        }
+    }
+}
