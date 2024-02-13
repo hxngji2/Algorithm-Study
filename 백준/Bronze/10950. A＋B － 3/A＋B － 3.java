2f63629ebb86a88e5bdab3a2948dfc633f83165b
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-02-13 22:56:10 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] sum = new int[t];
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum[i] = a + b;
+        }
+
+        for (int i = 0; i < t ; i++) {
+            System.out.println(sum[i]);
+        }
+
+    }
+}
