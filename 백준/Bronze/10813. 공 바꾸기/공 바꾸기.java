79891a5f71b0d37f26d5e7b3010e3cdca7e777c8
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-02-22 13:46:01 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] baskets = new int[N];
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            baskets[i] = i + 1;
+        }
+        for (int j = 0; j < M; j++) {
+            st = new StringTokenizer(br.readLine());
+
+            int I = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+
+            int temp = baskets[I-1];
+            baskets[I-1] = baskets[J-1];
+            baskets[J-1] = temp;
+        }
+        for (int i = 0; i < baskets.length; i++)
+            System.out.print(baskets[i] + " ");
+    }
+}
+
