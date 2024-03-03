b40d235415fecb30963ff49b60cc6bc4818412d6
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-03-03 22:06:40 +0900

    [Bronze II] Title: 복호화, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/9046. 복호화/복호화.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            String input = br.readLine();
+            int[] result = new int[26];
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) >= 'a' && input.charAt(i) <= 'z') {
+                    result[input.charAt(i) - 'a']++;
+                }
+            }
+
+            int max = 0;
+            for (int r : result){
+                if (r > max) {
+                    max = r;
+                }
+            }
+
+            int count = 0;
+            int answer = 0;
+            for (int j = 0; j < 26; j++) {
+                if (max == result[j]){
+                    count++;
+                    answer = j;
+                }
+            }
+
+            System.out.println(count == 1 ? (char) (answer + 'a') : "?");
+        }
+    }
+}
