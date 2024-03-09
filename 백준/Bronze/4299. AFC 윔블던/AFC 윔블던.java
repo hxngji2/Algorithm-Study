f3c9695625bca2edfa973880b3c9782189092d27
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-03-09 19:34:47 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
@@ -13,6 +14,8 @@ public class Main {
         if ((a + b) % 2 != 0 || a < b)
             System.out.println(-1);
         else
-            System.out.print((a + b) / 2 + " " + ((a + b) / 2 - b));
+            sb.append((a+b)/2).append(" ").append(((a + b) / 2 - b));
+            System.out.println(sb.toString());
+//            System.out.print((a + b) / 2 + " " + ((a + b) / 2 - b));
     }
 }
