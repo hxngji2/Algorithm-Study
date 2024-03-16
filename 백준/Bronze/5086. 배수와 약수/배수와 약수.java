947e397e5fe589e6b400edbd0c22b14a709fbb3b
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-03-16 15:37:34 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -6,21 +6,26 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        int first, second;
         while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
 
-            int first = Integer.parseInt(st.nextToken());
-            int second = Integer.parseInt(st.nextToken());
+            first = Integer.parseInt(st.nextToken());
+            second = Integer.parseInt(st.nextToken());
 
             if (first == 0 && second == 0) break;
 
             if (first % second == 0) {
-                System.out.println("multiple");
+                sb.append("multiple").append("\n");
             } else if (second % first == 0) {
-                System.out.println("factor");
+                sb.append("factor").append("\n");
             } else
-                System.out.println("neither");
+                sb.append("neither").append("\n");
         }
+
+        System.out.print(sb);
     }
 }
