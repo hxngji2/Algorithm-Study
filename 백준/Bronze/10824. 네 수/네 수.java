42e5247664742864af057db1d38f7597fbbdbfef
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-03-21 10:32:05 +0900

    [Bronze III] Title: 네 수, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10824. 네 수/네 수.java
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -5,15 +5,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         String[] num = br.readLine().split(" ");
 
         long ab = Long.parseLong(num[0] + num[1]);
         long cd = Long.parseLong(num[2] + num[3]);
 
-        sb.append(ab + cd);
-        System.out.print(sb);
+        System.out.print(ab + cd);
     }
 }
 
