a5771fc3f2b1bc5e11554a08e569099d2b8b9147
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-03-23 23:11:41 +0900

    [Bronze III] Title: 대지, Time: 344 ms, Memory: 29976 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        System.out.println((x[x.length-1] - x[0]) * (y[y.length-1] - y[0]));
+
+    }
+}
