8cc835ffcd5fb8e068eee64fd02f25bf9cd2adf5
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-05-15 20:22:44 +0900

    [D1] Title: 신문 헤드라인, Time: 99 ms, Memory: 23,832 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase());
+    }    
+}
