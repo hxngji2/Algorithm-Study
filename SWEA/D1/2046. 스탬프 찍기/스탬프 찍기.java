f96c5fefaf1917c36d65c6ab293fd69523eee71a
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-05-15 23:50:11 +0900

    [D1] Title: 스탬프 찍기, Time: 104 ms, Memory: 23,888 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            sb.append("#");
+        }
+        System.out.println(sb);
+    }
+}
