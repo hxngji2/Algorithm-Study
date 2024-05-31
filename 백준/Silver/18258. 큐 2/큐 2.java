2d4911959b574d31550f773792f585ad2c578739
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-05-31 21:30:12 +0900

    [Silver IV] Title: 큐 2, Time: 1472 ms, Memory: 333604 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st;
+
+    Deque<Integer> queue = new ArrayDeque<>();
+    int N = Integer.parseInt(br.readLine());
+    int last = 0;
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine(), " ");
+
+      switch (st.nextToken()) {
+        case "push":
+          last = Integer.parseInt(st.nextToken());
+          queue.offer(last);
+          break;
+
+        case "pop":
+          if (queue.isEmpty()) {
+            sb.append(-1).append("\n");
+          } else {
+            sb.append(queue.poll()).append("\n");
+          }
+          break;
+
+        case "size":
+          sb.append(queue.size()).append("\n");
+          break;
+
+        case "empty":
+          if (queue.isEmpty()) {
+            sb.append(1).append("\n");
+          } else {
+            sb.append(0).append("\n");
+          }
+          break;
+
+        case "front":
+          if (queue.isEmpty()) {
+            sb.append(-1).append("\n");
+          } else {
+            sb.append(queue.peekFirst()).append("\n");
+          }
+          break;
+
+        case "back":
+          if (queue.isEmpty()) {
+            sb.append(-1).append("\n");
+          } else {
+            sb.append(queue.peekLast()).append("\n");
+          }
+          break;
+
+      }
+    }
+    System.out.println(sb);
+  }
+}
