d559c968adddbf4293b687613437c77af0954ed5
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-08-20 15:47:52 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -11,12 +11,8 @@ public class Main {
     int N = Integer.parseInt(br.readLine());
 
     for (int i = 0; i < N; i++) {
-      for (int j = 0; j < i; j++) {
-        sb.append(" ");
-      }
-      for (int j = 1; j <= 2*(N-i)-1; j++) {
-        sb.append("*");
-      }
+      sb.append(" ".repeat(i));
+      sb.append("*".repeat(Math.max(0, 2 * (N - i) - 1)));
       sb.append("\n");
     }
     System.out.println(sb);
