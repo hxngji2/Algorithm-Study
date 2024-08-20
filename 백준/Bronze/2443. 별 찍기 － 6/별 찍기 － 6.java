9944ca4186eb738caf66e9ff2eccc961f7d0ad4b
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-08-20 15:45:24 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,25 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-        for(int i = 1; i <= N; i++){
+    int N = Integer.parseInt(br.readLine());
 
-            for(int j = 1; j < i; j++){
-                sb.append(" ");
-            }
-
-            for(int k = (2*N)-(i*2-1); k > 0; k--){
-                sb.append("*");
-            }
-
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }    
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < i; j++) {
+        sb.append(" ");
+      }
+      for (int j = 1; j <= 2*(N-i)-1; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
 }
