803fc3a3ea38e7d8af756e3b4e492625bd25ab69
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-08-25 13:56:20 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= N; i++) {
+      for (int j = 0; j < N-i; j++) {
+        sb.append(" ");
+      }
+      for (int j = 0; j < i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    for (int i = 1; i < N; i++) {
+      for (int j = 0; j < i; j++) {
+        sb.append(" ");
+      }
+      for (int j = 0; j < N-i; j++) {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+}
