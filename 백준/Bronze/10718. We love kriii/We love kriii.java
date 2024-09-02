d59a1b02d180632f82cc3d2988fd9a298d717c1c
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-09-02 14:02:31 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < 2; i++) {
+      sb.append("강한친구 대한육군\n");
+    }
+    System.out.println(sb);
+  }
+}
