d3b01b14a86b1989ff7e6ec7148fc76707187c42
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-10-22 14:48:21 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 13976 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,30 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        while (true) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-            if (a == 0 || b == 0 || c == 0) break;
+    while (true) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
 
-            if (a == b && b == c) {
-                System.out.println("Equilateral");
-            } else if (a + b <= c || b + c <= a || a + c <= b) {
-                System.out.println("Invalid");
-            } else if (a != b && b != c && a != c) {
-                System.out.println("Scalene");
-            } else
-                System.out.println("Isosceles");
-        }
+      if (a == 0 && b == 0 && c == 0) {
+        break;
+      }
+
+      if (a + b <= c || b + c <= c || c + a <= b) {
+        sb.append("Invalid\n");
+      } else if (a == b && b == c) {
+        sb.append("Equilateral\n");
+      } else if (a == b || b == c || c == a) {
+        sb.append("Isosceles\n");
+      } else {
+        sb.append("Scalene\n");
+      }
     }
+    System.out.println(sb);
+  }
 }
 
