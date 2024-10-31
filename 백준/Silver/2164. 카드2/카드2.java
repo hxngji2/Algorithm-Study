a08243d8f4528261e3ed7fe63e88913da4477adb
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-10-31 23:32:04 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 53500 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-  public static void main(String[] args)throws Exception{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    /*
+     * 결과: 제일 마지막에 남는 카드 숫자
+     * 싸이클: 1번을 버리면 나머지 숫자가 다시 정렬되고, 그때의 1번을 뺴서 맨뒤에 넣어줌
+     * size가 1일 때까지 반복
+     */
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-    Deque<Integer> queue = new ArrayDeque<>();
+        Queue<Integer> q = new LinkedList<>();
 
-    for (int i = 1; i <= N; i++){
-      queue.offer(i);
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+
+            if(q.size() > 1){
+                int num = q.poll();
+                q.offer(num);
+            }
+        }
+
+        System.out.println(q.peek());
     }
-
-    while (queue.size() > 1){
-      queue.poll();
-      queue.offer(queue.poll());
-    }
-
-    System.out.println(queue.poll());
-  }
 }
