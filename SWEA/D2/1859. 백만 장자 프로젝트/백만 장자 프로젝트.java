4427bd39d5d9eac5943230572783575632b6762f
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-11-10 11:58:48 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 478 ms, Memory: 140,052 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -9,36 +9,29 @@ public class Solution {
 
         int t = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= t; tc++) {
+        for(int tc = 1; tc <= t; tc++) {
             int n = Integer.parseInt(br.readLine());
-
-            int[] arr = new int[n];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int i = 0; i < n; i++) {
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            int start = 0;
+            int max = 0;
             long ans = 0;
-            while (start < n) {
-
-                int maxIdx = start;
-                for (int i = start + 1; i < n; i++) {
-                    if (arr[maxIdx] < arr[i])
-                        maxIdx = i;
+            for(int i = n-1; i >= 0; i--){
+                if(max < arr[i]) {
+                    max = arr[i];
+                } else {
+                    ans += max - arr[i];
                 }
-
-                for (int i = start; i < maxIdx; i++) {
-                    ans += arr[maxIdx] - arr[i];
-                }
-
-                start = maxIdx + 1;
+                
             }
             sb.append(String.format("#%d %d\n", tc, ans));
-        }
 
+        }
         System.out.println(sb);
     }
 }
