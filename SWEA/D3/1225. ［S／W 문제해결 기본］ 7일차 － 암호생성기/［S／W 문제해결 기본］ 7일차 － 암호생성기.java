b93bdca30e1a92f3316bd1030b9bf79f44dd1c92
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-11-12 23:28:21 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 128 ms, Memory: 20,920 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -5,41 +5,44 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Solution {
-  static Deque<Integer> queue;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = 10;
-    for (int tc = 1; tc <= T; tc++) {
-      int num = Integer.parseInt(br.readLine());
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      queue = new ArrayDeque<>();
-      for (int i = 0; i < 8; i++) {
-        queue.offer(Integer.parseInt(st.nextToken()));
-      }
-      cycle();
-      StringBuilder sb = new StringBuilder();
-      sb.append("#").append(tc).append(" ");
-      for (int i = 0; i < 8; i++) {
-        sb.append(queue.poll()).append(" ");
-      }
-      sb.append("\n");
-      System.out.println(sb);
+        int T = 10;
+
+        while(T-- > 0){
+            int tc = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            Deque<Integer> q = new ArrayDeque<>();
+
+            for(int i = 0; i < 8; i++){
+                q.offer(Integer.parseInt(st.nextToken()));
+            }            
+
+            int n = 1;
+
+            while(true){
+                int current = q.poll() - n;
+
+                if(current <= 0){
+                    q.offer(0);
+                    break;
+                } else {
+                    q.offer(current);
+                }
+                n = (n % 5) + 1;
+            }
+
+            sb.append(String.format("#%d ",tc));
+
+            while(!q.isEmpty()){
+                sb.append(q.poll()).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
-  }
-
-  public static void cycle() {
-    int index = 1;
-    while (true) {
-      int current = queue.poll() - index;
-
-      if (current <= 0) {
-        queue.offer(0);
-        break;
-      } else {
-        queue.offer(current);
-      }
-      index = (index % 5) + 1;
-    }
-  }
 }
