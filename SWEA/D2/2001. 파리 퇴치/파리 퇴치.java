07a8b751386491041885b66f35065feca6c4ad69
홍지원 <96656059+hxngji2@users.noreply.github.com>
2024-11-13 19:43:30 +0900

    [D2] Title: 파리 퇴치, Time: 115 ms, Memory: 18,252 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,14 +1,15 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
+        for(int tc = 1; tc <= t; tc++){
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int n = Integer.parseInt(st.nextToken());
@@ -16,29 +17,29 @@ public class Solution {
 
             int[][] board = new int[n][n];
 
-            for (int i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++){
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
+                for(int j = 0; j < n; j++){
                     board[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
+            int sum = 0;
             int max = 0;
-            int fly = 0;
-            for (int i = 0; i < n - m + 1; i++) {
-                for (int j = 0; j < n - m + 1; j++) {
-                    for(int k = i; k < i+m; k++){
-                        for(int o = j; o < j+m; o++){
-                            fly += board[k][o];
+            for(int i = 0; i < n-m+1; i++){
+                for(int j = 0; j < n-m+1; j++){
+                    for(int k = i; k < m+i; k++){
+                        for(int r = j; r < m+j; r++){
+                            sum += board[k][r];
                         }
                     }
-                    if (fly > max)
-                        max = fly;
-                    fly = 0;
+                    max = Math.max(max,sum);
+                    sum = 0;
                 }
             }
 
-            sb.append(String.format("#%d %d\n", tc, max));
+            sb.append(String.format("#%d %d\n",tc,max));
+
         }
         System.out.println(sb);
     }
